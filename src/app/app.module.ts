708107714a830c2154decb403e23bfb856be1f49
Ajay Kumbhar <98207015+nobitanobi01@users.noreply.github.com>
2022-10-18 20:31:40 +0530

    created project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './Components/home/home.component';
+import { AboutComponent } from './Components/about/about.component';
+import { FeedbackComponent } from './Components/feedback/feedback.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AboutComponent,
+    FeedbackComponent
   ],
   imports: [
     BrowserModule,
