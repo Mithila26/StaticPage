871cd57822f5da80e95881830736205f3d0213c2
Mithila26 <mithila.prabhu@lntinfotech.com>
2022-10-23 15:36:37 +0530

    feedback
    feedback

--- src/app/Components/feedback/feedback.component.ts
+++ src/app/Components/feedback/feedback.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, FormControl, FormArray, Validators } from '@angular/forms';
+import { Observable } from 'rxjs';
+
+
 
 @Component({
   selector: 'app-feedback',
@@ -6,10 +10,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./feedback.component.css']
 })
 export class FeedbackComponent implements OnInit {
-
+  feedbackform = new FormGroup({
+    firstName: new FormControl('', Validators.required),
+    title: new FormControl('', Validators.required),
+    lastName: new FormControl('', Validators.required),
+    email: new FormControl('', [
+      Validators.required,
+      Validators.email
+    ])
+    
+   });
   constructor() { }
-
-  ngOnInit(): void {
+  get firstname(){
+    return this.feedbackform.get('firstName')
   }
-
+  ngOnInit(): void {
+    
+  }
+  onSubmit(data: any){
+    //alert(JSON.stringify(this.feedbackform.value));
+    //if(this.feedbackform.valid){
+    alert("Thank You for your Feedback");
+    console.log("Form Submitted!");
+    this.feedbackform.reset();//}
+  }
+  clearForm() {
+    this.feedbackform.reset(); // Resets the formgroup
+  }
 }
