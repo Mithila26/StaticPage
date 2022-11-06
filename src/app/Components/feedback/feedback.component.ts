c4b1afbd54a630e8029ea2dabb023b50c5961d64
MP <mithila.prabhu@lntinfotech.com>
2022-11-06 15:50:26 +0000

    Update 3 files

--- src/app/Components/feedback/feedback.component.ts
+++ src/app/Components/feedback/feedback.component.ts
@@ -1,39 +1,40 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormBuilder, FormControl, FormArray, Validators } from '@angular/forms';
+import {
+  FormGroup,
+  FormBuilder,
+  FormControl,
+  FormArray,
+  Validators,
+} from '@angular/forms';
 import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-feedback',
   templateUrl: './feedback.component.html',
-  styleUrls: ['./feedback.component.css']
+  styleUrls: ['./feedback.component.css'],
 })
 export class FeedbackComponent implements OnInit {
   feedbackform = new FormGroup({
     firstName: new FormControl('', Validators.required),
     title: new FormControl('', Validators.required),
     lastName: new FormControl('', Validators.required),
-    email: new FormControl('', [
-      Validators.required,
-      Validators.email
-    ]),
-    feedback: new FormControl()
-
+    email: new FormControl('', [Validators.required, Validators.email]),
+    feedback: new FormControl(),
   });
-  constructor() { }
+  constructor() {}
   get firstname() {
-    return this.feedbackform.get('firstName')
+    return this.feedbackform.get('firstName');
   }
-  ngOnInit(): void {
-
-  }
+  ngOnInit(): void {}
   onSubmit(data: any) {
     //alert(JSON.stringify(this.feedbackform.value));
     //if(this.feedbackform.valid){
-    alert("Thank You for your Feedback");
-    console.log("Form Submitted!");
-    this.feedbackform.reset();//}
+    alert('Thank You for your Feedback');
+    console.log('Form Submitted!');
+    this.feedbackform.reset(); //}
   }
   clearForm() {
     this.feedbackform.reset(); // Resets the formgroup
+    this.answer = null;
   }
 }
