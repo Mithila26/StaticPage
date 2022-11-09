340c5271aa5133df9b442f13d13644270da63b2a
MP <mithila.prabhu@lntinfotech.com>
2022-11-09 17:40:52 +0000

    Update 10 files

---
+++ src/app/Components/models/user.ts
@@ -0,0 +1,10 @@
+export interface User {
+    Name: string;
+    UserID: string;
+    email_address: string;
+    phone_number: string;
+    country: string;
+    county: string;
+    sub_county: string;
+    constituency: string;
+}
