7f472ba504241d31627a3316903617ea5a54b5fd
Mithila26 <113879650+Mithila26@users.noreply.github.com>
2022-11-11 12:00:52 +0530

    Update claims.component.ts

--- src/app/Components/claims/claims.component.ts
+++ src/app/Components/claims/claims.component.ts
@@ -4,7 +4,8 @@ import {
   FormBuilder,
   FormControl,
   FormArray,
-  Validators,
+  Validators
+  
 } from '@angular/forms';
 import { Observable } from 'rxjs';
 
@@ -14,7 +15,35 @@ import { Observable } from 'rxjs';
   styleUrls: ['./claims.component.css'],
 })
 export class ClaimsComponent implements OnInit {
-  constructor() {}
+  isLinear = true;
+  //firstFormGroup: FormGroup;
+  //secondFormGroup: FormGroup;
 
-  ngOnInit(): void {}
+  firstFormGroup = this._formBuilder.group({
+    name: ['', Validators.required],
+    description: ['', Validators.required]
+  });
+  secondFormGroup = this._formBuilder.group({
+    amount: ['', Validators.required],
+    stock: ['', Validators.required]
+  });
+  constructor(private _formBuilder: FormBuilder) {}
+
+  
+
+  ngOnInit(): void {
+    /*this.firstFormGroup = this._formBuilder.group({
+      name: ['', Validators.required],
+      description: ['', Validators.required]
+    });
+    this.secondFormGroup = this._formBuilder.group({
+      amount: ['', Validators.required],
+      stock: ['', Validators.required]
+    });*/
+  }
+
+  submit(){
+    console.log(this.firstFormGroup.value);
+    console.log(this.secondFormGroup.value);
+}
 }
